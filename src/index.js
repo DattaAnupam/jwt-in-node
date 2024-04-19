e7d4a13c858863ed47ab68942a69b7c283ca5dad
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-04-19 14:34:25 +0530

    sign the payload

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
-const num = 10;
-console.log(num);
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const APP = express();
+const PORT = process.env.PORT || 3000;
+const posts = [
+    {
+        title: 'First post',
+        date: new Date(),
+        content: 'This is the first post'
+    },
+    {
+        title: 'Second post',
+        date: new Date(),
+        content: 'This is the second post'
+    }
+]
+
+APP.use(express.json());
+APP.get('/posts', (_, res, next) => {
+    res.send(posts);
+
+    next();
+})
+
+APP.post('/getToken', (req, res) => {
+    console.log('Request received ' + JSON.stringify(req.body));
+    const username = req.body.username;
+    const user = {
+        name: username
+    }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+    res.json({ accessToken: accessToken });
+})
+
+APP.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/typedef/index.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} User
+ * @property {string} username - The username of the user.
+ * @property {string} email - The email address of the user.
+ * @property {Post} post - The post created by the user.
+ */
+
+/**
+ * @typedef {Object} Post
+ * @property {string} title - The title of the post.
+ * @property {string} content - The content of the post.
+ */
+
