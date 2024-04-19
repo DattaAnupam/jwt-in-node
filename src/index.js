100fe0a3743c515b8fafe4e2229304629d8e3151
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-04-19 16:00:50 +0530

    add middleware that verifies access token

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
+import authenticateToken from './middleware/index.js';
+
 dotenv.config();
 const APP = express();
+
 const PORT = process.env.PORT || 3000;
 const posts = [
     {
@@ -22,19 +25,36 @@ const posts = [
 
 APP.use(express.json());
 
-APP.get('/posts', (req, res) => {
-    const result = posts.filter(post => post.user === req.body.username);
+/**
+ * This is a sample GET API which returns the first post of the user
+ * The user is identified by the token sent in the request header
+ * If the token is not present, the API returns 401
+ * If the token is invalid, the API returns 403
+ * If the user is not found, the API returns 404
+ * If the user is found, the API returns the first post of the user
+ */
+APP.get('/posts', authenticateToken, (req, res) => {
+    // filter the posts based on the valid user
+    const result = posts.filter(post => post.user === req.user.name);
+
+    // if no post is found, return 404
     if(result == undefined || result.length == 0) {
         res.set('content-type', 'application/json')
             .status(404)
             .send({message: 'No post found'});
     }
 
+    // return the first post
     else {
         res.send({"title": result[0].title, "content": result[0].content});
     }
 })
 
+/**
+ * This is a sample POST API which returns the token for the user
+ * The user is identified by the username sent in the request body
+ * The token is generated using the username and sent back to the client
+ */
 APP.post('/getToken', (req, res) => {
     console.log('Request received ' + JSON.stringify(req.body));
     const username = req.body.username;
@@ -42,11 +62,14 @@ APP.post('/getToken', (req, res) => {
         name: username
     }
 
+    // generate token using user details sent
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
+    // send the token back to the client
     res.json({ accessToken: accessToken });
 })
 
+// start the server
 APP.listen(3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ src/middleware/index.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+/**
+ * Middleware function to authenticate the token in the request header.
+ * If the token is valid, it sets the user in the request object and calls the next middleware.
+ * If the token is missing or invalid, it returns an error response.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Object} - The response object or calls the next middleware.
+ */
+const authenticateToken = (req, res, next) => {
+    // get the token from the request header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    // if the token is not present, return unauthorized
+    if(token == null) {
+      return res.status(401).send({message: 'Unauthorized user'});
+    }
+  
+    // verify the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if(err) {
+        return res.status(403).send({message: 'Invalid Token'}); // if the token is invalid, return forbidden
+      }
+  
+      // set the user in the request object
+      req.user = user;
+      
+      next();
+    })
+};
+
+export default authenticateToken;
